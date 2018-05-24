a4574f7ee073e1aa79b3a55389a5328898602d17
Rod Johnson <johnsonroda@gmail.com>
2018-05-23 22:34:22 -0700

    Update Nish2Controller.java

--- src/main/java/com/ippon/Nish2Controller.java
+++ src/main/java/com/ippon/Nish2Controller.java
@@ -22,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import java.util.File;
+
 @RestController
 class Nish2Controller {
 
